7085801118aa86221764762343fd32a2ea191c01
Aaron Bopp <bopp.aa.bis@gmail.com>
2022-01-18 11:17:20 -0600

    create updateEditTime

---
+++ 00_Meta/Converters/updateEditTime.py
@@ -0,0 +1,55 @@
+import os
+import re
+import json
+from datetime import date
+
+def get_files_sizes(directory):
+    """
+    Store file sizes in a dict
+    """
+    file_sizes = dict()
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.md'):
+                filepath = os.path.join(root, filename)
+                file_sizes[filepath] = os.path.getsize(filepath)
+    return file_sizes
+
+def dump_current_file_sizes(directory):
+    # dump file sizes to json
+    file_sizes = get_files_sizes(directory)
+    print(os.getcwd())
+    with open('file_sizes.json', 'w') as f:
+        json.dump(file_sizes, f)
+
+def get_files_sizes_from_json():
+    # get file sizes from json
+    with open('file_sizes.json', 'r') as f:
+        file_sizes = json.load(f)
+    return file_sizes
+
+def update_edit_time(path, date):
+    # update edit time
+    with open(path, 'r') as f:
+        text = f.read()
+    text = re.sub(r'\n\*edited .*\*\n', f'*edited {date}*\n', text)
+    with open(path, 'w') as f:
+        f.write(text)
+        
+if __name__ == '__main__':
+    directory = r'C:\Users\aweso\Documents\GitHub\Obsidian-Notes\TIM'
+    file_sizes = get_files_sizes_from_json()
+    current_file_sizes = get_files_sizes(directory)
+    # get todays date in YYYY-MM-DD format
+    current_date = date.today().strftime("%B %d, %Y")
+    for current in current_file_sizes:
+        if current not in file_sizes:
+            update_edit_time(current, current_date)
+            print(f'{current} updated')
+        else:
+            if file_sizes[current] > current_file_sizes[current] + 100 or file_sizes[current] < current_file_sizes[current] - 100:
+                update_edit_time(current, current_date)
+                print(f'{current} updated')
+        
+
+    dump_current_file_sizes(directory)
