f7e003aaf0f20d3607234170121a11f1e39cfc5b
Aaron Bopp <bopp.aa.bis@gmail.com>
2022-01-18 13:03:49 -0600

    vault backup: 2022-01-18 13:03:48

--- 00_Meta/Converters/updateIO.py
+++ 00_Meta/Converters/updateIO.py
@@ -6,26 +6,25 @@ def get_IO_all_files(directory):
     file_io = dict()
     for root, dirs, files in os.walk(directory):
         for filename in files:
-            if filename.endswith('.md'):
+            filepath = os.path.join(root, filename)
+            if filename.endswith('.md') and '00_Meta' not in filepath and '10_Sources' not in filepath: 
                 name = filename.replace('.md', '')
-                file_io[name] = {'inlinks': set(), 'outlinks': set(), 'path': os.path.join(root, filename)}
-    # walk through directoryk
+                file_io[name] = {'inlinks': set(), 'outlinks': set(), 'path': filepath}
     for root, dirs, files in os.walk(directory):
         for filename in files:
-            if filename.endswith('.md'):
-                filepath = os.path.join(root, filename)
+            filepath = os.path.join(root, filename)
+            if filename.endswith('.md') and '00_Meta' not in filepath and '10_Sources' not in filepath: 
                 with open(filepath, 'r', encoding='utf-8') as f:
                     text = f.read()
                 name = filename.replace('.md', '')
-                links = re.findall(r'\[\[([^\]\|\#]+)[\#\|]?[^\]]*?\]\]', text)
-                try:
-                    for m in links:
-                        print(m[1])
-                        if m[1] in file_io.keys():
-                            file_io[m[1]]['inlinks'].add(name)
-                        file_io[name]['outlinks'].add(m[1])
-                except IndexError:
-                    pass
+                links = re.findall(r'\[\[([^\]\|\#]+)', text)
+                for m in links:
+                    if m in file_io.keys():
+                        file_io[m]['inlinks'].add(name)
+                    file_io[name]['outlinks'].add(m) 
+    for file in file_io:
+        file_io[file]['inlinks'] = list(file_io[file]['inlinks'])
+        file_io[file]['outlinks'] = list(file_io[file]['outlinks'])
     return file_io
 
 if __name__ == '__main__':
